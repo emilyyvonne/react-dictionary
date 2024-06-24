ea1a5ad0a6564e6c518f72864172d3317964d331
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2024-06-23 20:38:16 -0400

    changed to li

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,7 @@ export default function Synonyms(props) {
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return;
-          <span key={index}>{synonym}</span>;
+          <li key={index}>{synonym}</li>;
         })}
       </ul>
     );
