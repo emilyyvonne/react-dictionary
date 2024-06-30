6e05e9162cd52208d0cc1695f4abfa6ff342f272
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2024-06-30 13:37:01 -0400

    created phonetics component

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,14 @@
 import Reach from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function Result(props) {
-  console.log("results:" + props.results);
   return (
     <div className="Results">
       {props.results && (
         <>
           <h2>{props.results.word}</h2>
+          <Phonetic props={props.results.phonetic} />
           {props.results.meanings &&
             props.results.meanings.map(function (meaning, index) {
               return (
