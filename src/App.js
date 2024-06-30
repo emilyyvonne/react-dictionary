7796ca2bb37f98e969d6d2ee96669a454f5aa385
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2024-06-30 15:23:41 -0400

    adding photo grid

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">DICTIONARY</header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer>
-        this dictionary application was coded by <a href=" ">Emily Brisbin</a>
-      </footer>
+      <div className="container">
+        <header className="App-header">DICTIONARY</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          this dictionary application was coded by{" "}
+          <a href="https://github.com/emilyyvonne">Emily Brisbin</a> and is
+          hosted by{" "}
+          <a href="https://majestic-truffle-50d583.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
